17dfacae64d3eb7a21701ff2fa26611f8ae8f199
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2020-12-21 21:31:00 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require("path");
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const env = process.env.NODE_ENV;
+
+module.exports = {
+  entry: "./js/app.js",
+
+  mode: env,
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+    publicPath: "/",
+  },
+
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname, "public"),
+        watch: true,
+      },
+    ],
+    port: 3000,
+    hot: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          env == "development" ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./index.html" }),
+    new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
+};
