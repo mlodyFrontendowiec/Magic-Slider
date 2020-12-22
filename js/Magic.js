193015aee4dfd528755c8dada5196f2d16222e16
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2020-12-22 12:38:47 +0100

    Add handle the scroller

---
+++ js/Magic.js
@@ -0,0 +1,60 @@
+export class Magic {
+  constructor() {
+    this.imagesContainerEl = document.querySelector(
+      ".slider__images-container"
+    );
+    this.img1El = document.querySelector(".slider__image-container--first img");
+    this.img2El = document.querySelector(
+      ".slider__image-container--second img"
+    );
+    this.dragging = false;
+    this.imagesContainerLeftOffset = this.imagesContainerEl.offsetLeft;
+
+    this.handleEl = document.querySelector(".slider__handle");
+    this.dividerEl = document.querySelector(".slider__divider");
+    this.imagesContainerWidth = this.imagesContainerEl.offsetWidth;
+  }
+
+  initEvents() {
+    window.addEventListener("resize", () => {
+      this.handleResize();
+    });
+    this.handleEl.addEventListener("mousedown", () => {
+      this.dragging = true;
+    });
+    this.handleEl.addEventListener("mouseup", () => {
+      this.dragging = false;
+    });
+    window.addEventListener("mousemove", (event) => {
+      if (this.dragging) {
+        this.move(event.clientX);
+      }
+    });
+  }
+  getOffset(clientX) {
+    const offset = clientX - this.imagesContainerLeftOffset;
+    console.log(clientX);
+    if (offset < 0) {
+      return 0;
+    } else if (offset > this.imagesContainerWidth) {
+      return this.imagesContainerWidth;
+    } else {
+      return offset;
+    }
+  }
+
+  move(clientX) {
+    const offset = this.getOffset(clientX);
+    console.log(offset);
+    this.dividerEl.style.left = offset + "px";
+    this.showImage();
+  }
+  handleResize() {
+    this.imagesContainerWidth = this.imagesContainerEl.offsetWidth;
+    this.imagesContainerLeftOffset = this.imagesContainerEl.offsetLeft;
+    this.img1El.style.width = this.imagesContainerLeftOffset + "px";
+    this.img2El.style.width = this.imagesContainerLeftOffset + "px";
+    console.log(this.imagesContainerWidth);
+    console.log(this);
+  }
+}
--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
 import "../sass/style.scss";
+import { Magic } from "./Magic";
+document.addEventListener("DOMContentLoaded", () => {
+  const scroller = new Magic();
+  scroller.initEvents();
+});
